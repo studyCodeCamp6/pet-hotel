9e19762e012a545508584083966ae6ae2206e270
mercury92 <pisanu.prm@gmail.com>
2020-08-24 18:58:56 +0700

    try to fix conflict inorder to pull P.jit backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,34 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+const passport = require("passport");
+
+
+let allowdOrgins = ["http://localhost:3000"];
+
+const userRoutes = require("./routes/user");
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowdOrgins.indexOf(origin) === -1) {
+        let mes = "you can not access";
+        return callback(new Error(mes), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/user", userRoutes);
+
 // db.sequelize.sync({ force:false})
 // .then(() => {
-  app.listen(process.env.PORT,
-    () => {
-      console.log(`server is running at port:${process.env.PORT}`);
-    });
+app.listen(process.env.PORT, () => {
+  console.log(`server is running at port:${process.env.PORT}`);
+});
 // }).catch(err => console.log(err.message));
