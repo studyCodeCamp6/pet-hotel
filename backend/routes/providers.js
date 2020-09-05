b173d30a8b637ad3e33a034208ee53b946fb0281
Pongsatorn <tackpongsatorn@gmail.com>
2020-09-05 22:49:22 +0700

    [update]connect table all provider

--- backend/routes/providers.js
+++ backend/routes/providers.js
@@ -9,5 +9,6 @@ router.post("/add", auth, controllers.register);
 router.get('/',auth,controllers.getProvider)
 router.get('/service',auth,controllersGetService.getService)
 router.delete('/service/:id',auth,controllersGetService.deleteService)
+router.put('/service/:id',auth,controllers.updateProvider)
 
 module.exports = router
