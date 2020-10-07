f72d6f5395bb3dd7ae775591f77397d2d497c131
mine01 <patteera.banl@gmail.com>
2020-10-07 16:21:47 +0700

    fixed navbar

--- frontend/src/containers/component/Navbar/Navbar.js
+++ frontend/src/containers/component/Navbar/Navbar.js
@@ -11,27 +11,30 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="NavbarItems">
-        <h1 className="navbar-logo" href="#">
-          Haustier's Hotel
-        </h1>
-        <div className="menu-icon" onClick={this.handleClick}>
-          <i
-            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
-          ></i>
-        </div>
-        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <a className={item.cName} href={item.url}>
-                  {item.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+      <div className="fixed-scroll">
+        <nav className="NavbarItems">
+          <h1 className="navbar-logo" href="#">
+            Haustier's Hotel
+          </h1>
+          <div className="menu-icon" onClick={this.handleClick}>
+            <i
+              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </div>
+          
+          {this.state.clicked?<ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+            {MenuItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <a className={item.cName} href={item.url}>
+                    {item.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>:""}
+        </nav>
+      </div>
     );
   }
 }
